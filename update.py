80de984f94bd63fe4574cfdd5e6c49a84cee5d07
Ziwei Huang <huang-ziwei@outlook.com>
2023-04-27 23:41:43 +0200

    update

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ from glob import glob
 
 # list 26 alphebets
 inits = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-entries = glob("./Hoisanwa/words/*/*.md")
+entries = sorted(glob("./Hoisanwa/words/*/*.md"))
 inits_exist = [init for init in inits if any("/"+init+"/" in entry for entry in entries)]
 with open("Hoisanwa/Home.md", 'w') as f:
     f.write("# 首页 / Home\n\n")
@@ -14,25 +14,9 @@ with open("Hoisanwa/Home.md", 'w') as f:
             s = ""
             for i, entry in enumerate(entries):
                 if "/"+init+"/" in entry:
-                    if i==0 or i % 10 != 0:
-                        s += f"[[{entry.split('/')[-1].split('.')[0]}]] "
-                    else:
+                    if i!=0 and i % 10 == 0:
                         s += f"\n[[{entry.split('/')[-1].split('.')[0]}]] "
+                    else:
+                        s += f"[[{entry.split('/')[-1].split('.')[0]}]] "
             f.write(s + "\n\n")
-        
-
-
-# with open("Hoisanwa/Home.md", 'w') as f:
-#     f.write("# Home\n\n")
-
-#     for init in inits:
-#         f.write(f"## {init}\n")
-#         for entry in entries:
-#             if "/"+init+"/" in entry:
-#                 filename = entry.split("/")[-1].split(".")[0]
-#                 f.write(f"[[{filename}]]\n")
-#         f.write("\n\n")
-    
-                
-
         
